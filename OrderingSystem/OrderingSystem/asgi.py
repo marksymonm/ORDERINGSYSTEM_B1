d1cd684a2947e5384c5b5972f1d21d679d2a5260
marksymonm <marksymonmartinez05@gmail.com>
2025-09-16 21:39:02 +0800

    Update asgi.py

--- OrderingSystem/OrderingSystem/asgi.py
+++ OrderingSystem/OrderingSystem/asgi.py
@@ -3,16 +3,14 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-# Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OrderingSystem.settings')
 
-# Standard ASGI application for HTTP
+# Assign ASGI app for HTTP
 django_asgi_app = get_asgi_application()
 
-# Import websocket URL patterns AFTER settings are configured
+# Import websocket patterns after settings are configured
 from MSMEOrderingWebApp.routing import websocket_urlpatterns
 
-# ProtocolTypeRouter to handle HTTP + WebSockets
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
