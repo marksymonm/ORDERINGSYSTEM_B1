f66dd4445ffbf2e8438f13d2cf7bde9bfd6b5e4b
marksymonm <marksymonmartinez05@gmail.com>
2025-09-16 23:42:38 +0800

    Update settings.py

--- OrderingSystem/OrderingSystem/settings.py
+++ OrderingSystem/OrderingSystem/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_ROOT = '/mnt/Media/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'MSMEOrderingWebApp/static')
