09976b9383f44ddbd81932d6fbb5372b55ff29ab
marksymonm <marksymonmartinez05@gmail.com>
2025-09-16 21:50:51 +0800

    Update asgi.py

--- OrderingSystem/OrderingSystem/asgi.py
+++ OrderingSystem/OrderingSystem/asgi.py
@@ -5,10 +5,8 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OrderingSystem.settings')
 
-# Assign ASGI app for HTTP
 django_asgi_app = get_asgi_application()
 
-# Import websocket patterns after settings are configured
 from MSMEOrderingWebApp.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
